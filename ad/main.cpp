937bc331f81b9476468b37609f27bfeeb49fbbc1
이진훈 <ppp123@ijinhun-ui-macbook-pro.local>
2019-06-14 13:27:10 +0900

    Initial Commit

---
+++ ad/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ad
+//
+//  Created by 이진훈 on 2019. 6. 14..
+//  Copyright © 2019년 hi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
